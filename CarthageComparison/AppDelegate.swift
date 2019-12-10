59dfd8e61fee9739067724d43c8cd5a6630f515c
Renan Silveira <rnn.silveira@gmail.com>
2019-12-10 15:35:12 -0300

    First commit

---
+++ CarthageComparison/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  CarthageComparison
+//
+//  Created by Renan Silveira on 10/12/19.
+//  Copyright © 2019 Renan Silveira. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
